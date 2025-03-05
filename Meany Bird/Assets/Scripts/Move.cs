8d50ebfd43feff86211f5d936ff27fd14cfbde71
MasterHacker10 <145071813+AmandaChen30@users.noreply.github.com>
2025-03-05 17:49:51 -0500

    codeing is cool

---
+++ Meany Bird/Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [Header("default Speed")]
+
+    public float speed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

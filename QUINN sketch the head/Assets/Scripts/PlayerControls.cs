02a618c97d91e588d94576612373179ee2968a98
MasterHacker10 <145071813+AmandaChen30@users.noreply.github.com>
2025-03-20 17:49:45 -0400

    bruh its a head

---
+++ QUINN sketch the head/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Rigidbody Settings")]
+    [Tooltip("Rigidbody2D object that is stored")]
+    public Rigidbody2D rb;
+
+    [Tooltip("Downward speed of the object")]
+    public float downSpeed = 20f;
+
+    [Header("Movement Settings")]
+    [Tooltip("Movement speed of the objects")]
+    public floats movementSpeed = 10f;
+
+    [Tooltip("Movent direction of the object")]
+    public float movement = 0f;
+
+    private SpriteRenderer spriteRenderer; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        spriteRenderer.flipX = movement < 0;
+    }
+    private void FixedUpdate() {
+        Vector2 velocity = rb.velocity;
+
+        velocity.x = movement;
+
+        rb.velocity = velocity; 
+    }
+}

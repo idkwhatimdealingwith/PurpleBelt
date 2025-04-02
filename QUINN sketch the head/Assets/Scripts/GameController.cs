02574a519264fd2f665fb6b591da60d8718024a6
MasterHacker10 <aleks.strzelecki@icloud.com>
2025-04-02 17:13:30 -0400

    coded the head

--- QUINN sketch the head/Assets/Scripts/GameController.cs
+++ QUINN sketch the head/Assets/Scripts/GameController.cs
@@ -12,12 +12,31 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        if (!instance) 
+            {
+            instance = this;
+            SpawnInitialPlatforms(); 
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void SpawnInitialPlatforms() 
+        {
+        for (int i = 0; i < 100; i++) 
+            {
+            SpawnPlatform(); 
+        }
+    }
+    private void SpawnPlatform() 
     {
-
+        float xPosition = Random.Range(-3f, 3f);
+        Instantiate(instance.platform, new Vector3(xPosition, instance.yPos, 0), Quaternion.identity);
+        instance.yPos += 2.5f; 
+    } 
+    public static void MovePlatformToTop(GameObject platform) 
+    {
+        float xPosition = Random.Range(-3f, 3f);
+        platform.transform.position = new Vector3(xPosition, instance.yPos, 0);
+        instance.yPos += 2.5f; 
     }
+
+    
 }

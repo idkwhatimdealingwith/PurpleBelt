7d272122968f51e70aaf49fa773d591639f5f331
MasterHacker10 <dylantaocallahan@gmail.com>
2025-04-09 17:53:35 -0400

    this is not funny unity

--- QUINN sketch the head/Assets/Scripts/Destroyer.cs
+++ QUINN sketch the head/Assets/Scripts/Destroyer.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnTriggerEnter2D(Collider2D collision) {
+        GameObject.Find("DoodleHead").SetActive(false);
+        GameController.GameOver(); 
     }
 }
--- QUINN sketch the head/Assets/Scripts/GameController.cs
+++ QUINN sketch the head/Assets/Scripts/GameController.cs
@@ -8,16 +8,40 @@ public class GameController : MonoBehaviour
 
     [Header("Platform Object")]
     public GameObject platform;
-    public float yPos = 0; 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float yPos = 0;
 
+    [Header("Game Over UI Canvas Object")]
+    public GameObject gameOverCanvas; 
+    void Start() {
+      if (!instance) 
+            {
+            instance = this;
+            SpawnInitialPlatforms(); 
+        }  
+    }
+    private void SpawnInitialPlatforms() 
+        { 
+     for (int i = 0; i < 100; i++) 
+            {
+            SpawnPlatform(); 
+        }
+    }
+    private void SpawnPlatform() 
+        {
+        float xPosition = Random.Range(-3f, 3f);
+        Instantiate(instance.platform, new Vector3(xPosition, instance.yPos, 0), Quaternion.identity);
+        instance.yPos += 2.5f; 
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void GameOver() 
     {
-
+        //Game Over canvas is set to active 
+        instance.gameOverCanvas.SetActive(true); 
+    }
+    public static void MovePlatformToTop(GameObject platform) 
+    {
+        float xPosition = Random.Range(-3f, 3f);
+        platform.transform.position = new Vector3(xPosition, instance.yPos, 0);
+        instance.yPos += 2.5f;
     }
 }

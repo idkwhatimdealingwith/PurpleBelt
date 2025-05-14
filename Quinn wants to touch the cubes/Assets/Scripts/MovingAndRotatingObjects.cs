8472f09456904faa3bcd073f90e06c0e8b1e7c3b
idkwhatimdealingwith <quinnsidell@icloud.com>
2025-05-14 18:00:14 -0400

    shnagins

--- Quinn wants to touch the cubes/Assets/Scripts/MovingAndRotatingObjects.cs
+++ Quinn wants to touch the cubes/Assets/Scripts/MovingAndRotatingObjects.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class MovingAndRotatingObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    [Header("Default Movement Speed")]
+    public float moveSpeed = 10f;
+    [Header("Default Rotation Speed")]
+    public float rotateSpeed = 50f;
     // Update is called once per frame
     void Update()
     {
-
+        transfrom.Translate(0, moveSpeed * Time.deltaTime, 0);
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime); 
     }
 }
--- Quinn wants to touch the cubes/Assets/Scripts/SpawnObjects.cs
+++ Quinn wants to touch the cubes/Assets/Scripts/SpawnObjects.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class SpawnObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    [Header("Spawn Cube Object")]
+    public GameObject spawnCube;
+    [Header("Default Difficulty")]
+    public float difficulty = 40f;
+    float spawn;
+   
     void Update()
     {
+        spawn += difficulty * Time.deltaTime;
+        while(spawn > 0)
+        {
+            spawn -= 1;
+            Vector3 v3Pos = transform.position + new Vector3(Random.value * 40f - 20f, 0, Random.value * 40f - 20f);
+            Quaternion qRotation = Quaternion.Euler(0, Random.value * 360f, Random.value *30f);
+        }
 
     }
+
+   
+    
 }
